756bf55e8c499d5d9b0d3517ad0a6d70010da2d5
Cain <nguyenhuutruong1303@gmail.com>
2019-04-20 10:44:00 +0700

    Push(10:42 - 20/4/2019)

---
+++ detect.py
@@ -0,0 +1,97 @@
+import cv2, numpy as np
+from PIL import Image
+import math,sys
+path = sys.argv[1]
+imgOriginal = cv2.imread('bienso/bienso/'+path)
+name = path.split('.')[0]
+#height, width, numChannels = imgOriginal.shape
+# module
+GAUSSIAN_SMOOTH_FILTER_SIZE = (5, 5)
+ADAPTIVE_THRESH_BLOCK_SIZE = 19
+ADAPTIVE_THRESH_WEIGHT = 9
+#preprocess
+def preprocess(imgOriginal):
+    imgGrayscale = extractValue(imgOriginal)
+    imgMaxContrastGrayscale = maximizeContrast(imgGrayscale)
+    height, width = imgGrayscale.shape
+    imgBlurred = np.zeros((height, width, 1), np.uint8)
+    imgBlurred = cv2.GaussianBlur(imgMaxContrastGrayscale, GAUSSIAN_SMOOTH_FILTER_SIZE, 0)
+    imgThresh = cv2.adaptiveThreshold(imgBlurred, 255.0, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                cv2.THRESH_BINARY_INV, ADAPTIVE_THRESH_BLOCK_SIZE, ADAPTIVE_THRESH_WEIGHT)
+
+    return imgGrayscale, imgThresh
+#extractValue
+def extractValue(imgOriginal):
+    height, width, numChannels = imgOriginal.shape
+    imgGrayscaleScene = np.zeros((height, width, 1), np.uint8)
+    imgThreshScene = np.zeros((height, width, 1), np.uint8)
+    imgContours = np.zeros((height, width, 3), np.uint8)
+    imgHSV = np.zeros((height, width, 3), np.uint8)
+    imgHSV = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2HSV)
+    imgHue, imgSaturation, imgValue = cv2.split(imgHSV)
+    return imgValue
+# maximizeContrast
+def maximizeContrast(imgGrayscale):
+    structuringElement = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    imgTopHat = cv2.morphologyEx(imgGrayscale, cv2.MORPH_TOPHAT, structuringElement)
+    imgBlackHat = cv2.morphologyEx(imgGrayscale, cv2.MORPH_BLACKHAT, structuringElement)
+    imgGrayscalePlusTopHat = cv2.add(imgGrayscale, imgTopHat)
+    imgGrayscalePlusTopHatMinusBlackHat = cv2.subtract(imgGrayscalePlusTopHat, imgBlackHat)
+    return imgGrayscalePlusTopHatMinusBlackHat
+
+def findplate(imgOriginal):
+    imgGrayscaleScene, imgThreshScene = preprocess(imgOriginal)
+    height, width, numChannels = imgOriginal.shape
+    plate = np.zeros((height,width,1),np.uint8)
+    _, contours,hierarchy = cv2.findContours(imgThreshScene, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        peri = cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, 0.06 * peri, True)
+        if (area >= 8000)and((len(approx)==8)or(len(approx)==4)):
+            #crop
+            x,y,w,h = cv2.boundingRect(contour)
+            plate = imgOriginal[y:y+h,x:x+w]
+            #cv2.imshow('img',new_img)
+    return plate
+
+#import class Char
+import Char
+def checkIfPossibleChar(Char):
+    if (Char.intBoundingRectArea > 30 and
+        Char.intBoundingRectWidth > 5 and Char.intBoundingRectHeight > 10 and
+        0.15 < Char.fltAspectRatio and Char.fltAspectRatio < 1.0):
+        return True
+    else:
+        return False
+def findChar(imgOriginal):
+
+    new_img = findplate(imgOriginal)
+    imgGrayscale, imgThresh = preprocess(new_img)
+    adaptivePlate = cv2.adaptiveThreshold(imgGrayscale,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,2)
+    blurPlate = cv2.GaussianBlur(adaptivePlate, (5,5),0)
+    ret, processedPlate = cv2.threshold(blurPlate,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    listOfPossibleChars = []
+    ind = 0
+    area_char = []
+    _,contours,_ = cv2.findContours(processedPlate,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        char = Char.Char(contour)
+        if (checkIfPossibleChar(Char.Char(contour))):
+            listOfPossibleChars.append(contour)
+            area_char.append(cv2.contourArea(contour))
+    #listOfPossibleChars.sort(key = lambda Char: Char.intCenterX)
+    avg = sum(area_char)/len(area_char)
+    for char in listOfPossibleChars:
+        if (cv2.contourArea(char)>avg):
+            x,y,w,h = cv2.boundingRect(char)
+            img_char = new_img[y:y+h,x:x+w]
+            ind += 1
+            #cv2.imshow(str(ind) ,img_char)
+            #cv2.imwrite('data/'+name+str(ind)+'.jpg',img_char)
+findChar(imgOriginal)
+#cv2.drawContours(new_img, listOfPossibleChars, -1, (0,255,0),3)
+#cv2.imshow('img',imgOriginal)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ train.py
@@ -0,0 +1,65 @@
+import keras
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.optimizers import RMSprop,Adam,SGD
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+
+img_width = 20
+img_height = 20
+train_data_dir = "data"
+validation_data_dir = "test"
+train_datagen = ImageDataGenerator(
+rescale = 1./255,
+horizontal_flip = True,
+fill_mode = "nearest",
+zoom_range = 0.1,
+width_shift_range = 0.3,
+height_shift_range=0.3,
+rotation_range=30)
+val_datagen = ImageDataGenerator(
+rescale = 1./255,
+horizontal_flip = True,
+fill_mode = "nearest",
+zoom_range = 0.1,
+width_shift_range = 0.3,
+height_shift_range=0.3,
+rotation_range=30)
+
+train_generator = train_datagen.flow_from_directory(
+train_data_dir,
+target_size = (img_height, img_width),
+batch_size = 5,
+class_mode = "categorical")
+validation_generator = val_datagen.flow_from_directory(
+validation_data_dir,
+target_size = (img_height, img_width),
+class_mode = "categorical")
+
+
+net = Sequential()
+net.add(Conv2D(32,kernel_size = 5, input_shape=(img_height,img_width,3), padding='same', activation='relu'))
+net.add(MaxPooling2D(pool_size=(2, 2)))
+net.add(Conv2D(64, kernel_size = 5, activation='relu', padding='same'))
+net.add(MaxPooling2D(pool_size=(2, 2)))
+net.add(Flatten())
+net.add(Dense(256, activation='relu'))
+net.add(Dense(128, activation='relu'))
+#net.add(Dropout(0.5))
+net.add(Dense(34, activation='softmax'))
+
+net.summary()
+
+checkpoint = ModelCheckpoint("model.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+early = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=1, mode='auto')
+net.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+net.fit_generator(train_generator, samples_per_epoch = 1025, epochs = 50, validation_data = validation_generator,
+                        nb_val_samples = 438, callbacks = [checkpoint, early])
+
+from keras.models import model_from_json
+model_json = net.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+net.save_weights("model.h5")
+print("Saved model to disk")
